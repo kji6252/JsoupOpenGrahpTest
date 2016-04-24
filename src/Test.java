1f781279d4ced5ad6a75ce9739d40e1c90d79767
김종인 <@192.168.102.41>
2016-04-24 19:41:54 +0900

    Jsoup를 활용한 오픈 그래프 메타 태그 가져 오기

---
+++ src/Test.java
@@ -0,0 +1,90 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		Document doc;
+		String url = "https://www.youtube.com/watch?v=B2z5C_Mesx0";
+		Map<String, List<String>> result = new HashMap<String,List<String>>();
+		
+		try {
+			doc = Jsoup.connect(url).get();
+			Elements ogElements = doc.select("meta[property^=og]");
+			for (Element e : ogElements) {
+				if(!result.containsKey(e.attr("property"))){
+					result.put(e.attr("property"), new ArrayList<String>());
+				}
+				result.get(e.attr("property")).add(e.attr("content"));
+			}
+			
+			for(String s : result.keySet())
+			System.out.println(s + " : " + result.get(s));
+			
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+
+	}
+	
+	
+	
+	
+	
+	
+	@org.junit.Test
+	public void test2() {
+		Document doc;
+		String url = "http://zzong.net";
+		Map<String, List<String>> result = new HashMap<String,List<String>>();
+		String[] REQUIRED_META = new String[]{"og:title", "og:type", "og:image", "og:url" };
+		
+		try {
+			doc = Jsoup.connect(url).get();
+			Elements ogElements = doc.select("meta[property^=og]");
+			for (Element e : ogElements) {
+				if(!result.containsKey(e.attr("property"))){
+					result.put(e.attr("property"), new ArrayList<String>());
+				}
+				result.get(e.attr("property")).add(e.attr("content"));
+			}
+			
+			for(String s : REQUIRED_META){
+				if (!(result.containsKey(s) && result.get(s).size() > 0)){
+					if(s.equals(REQUIRED_META[0])){
+						result.put(REQUIRED_META[0]
+								, Arrays.asList(new String[]{doc.select("title").eq(0).text()}));
+					} else if (s.equals(REQUIRED_META[1])){
+						result.put(REQUIRED_META[1]
+								, Arrays.asList(new String[]{"website"}));
+					} else if (s.equals(REQUIRED_META[2])){
+						result.put(REQUIRED_META[2]
+								, Arrays.asList(new String[]{doc.select("img").eq(0).attr("abs:src")}));
+					} else if (s.equals(REQUIRED_META[3])){
+						result.put(REQUIRED_META[3]
+								, Arrays.asList(new String[]{doc.baseUri()}));
+					}
+				}
+
+			}
+			
+			for(String s : result.keySet())
+			System.out.println(s + " : " + result.get(s));
+			
+	} catch (Exception e){
+		e.printStackTrace();
+	}
+
+	}
+
+}
